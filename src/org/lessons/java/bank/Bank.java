2e17fd96bf9953da84710d18bb29ad45e449213c
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-05 17:45:30 +0200

    Java Bank (not finished)

---
+++ src/org/lessons/java/bank/Bank.java
@@ -0,0 +1,37 @@
+package org.lessons.java.bank;
+
+import java.util.Scanner;
+public class Bank {
+    public static void main(String[] args){
+        String nome;
+        String opzione;
+        double versamento;
+        double prelievo;
+        int numConto;
+
+        numConto = (int)(Math.random()* 1001);
+
+        Scanner utente = new Scanner(System.in);
+        System.out.println("Puoi fornirmi il tuo nome?");
+        nome = utente.nextLine();
+
+        Conto contoUtente = new Conto(numConto, nome);
+
+        do{
+            System.out.println("Cosa vuoi fare?" + "\na)Versare denaro " + "\nb)Prelevare denaro" + "\nc)Uscire" + "\nScrivi la lettera corrispondente alla tua scelta");
+            opzione = utente.nextLine();
+
+            if(opzione.equals('a')){
+                System.out.println("Quanto vuoi versare?");
+                versamento = utente.nextLine();
+            } else if (opzione.equals('b')){
+                System.out.println("Quanto vuoi versare?");
+                prelievo = utente.nextLine();
+            } else if (opzione.equals('c')){
+                break;
+            } else {
+                System.out.println("Devi scegliere una delle tre opzioni");
+            }
+        }while (opzione != 'c');
+    }
+}
---
+++ src/org/lessons/java/bank/Conto.java
@@ -0,0 +1,49 @@
+package org.lessons.java.bank;
+
+import java.text.DecimalFormat;
+public class Conto {
+    //ATTRIBUTI
+    private int numConto;
+    private String nomeProprietario;
+    private double saldo;
+
+    //COSTRUTTORE
+    public Conto(int numConto, String nomeProprietario){
+        this.numConto = numConto;
+        this.nomeProprietario = nomeProprietario;
+        this.saldo = 0;
+    }
+
+    //GETTER
+    public int getNumConto() {
+        return this.numConto;
+    }
+
+    public String getNomeProprietario() {
+        return this.nomeProprietario;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    //SETTER
+    public void setNomeProprietario(String nome){
+        this.nomeProprietario = nome;
+    }
+
+    //METHODS
+    public void versaDeposito(double deposito){
+        this.saldo += deposito;
+    }
+
+    public void faiPrelievo(double prelievo){
+        if((this.saldo - prelievo) >= 0) this.saldo -= prelievo;
+        else System.out.println("Se prelevi così tanto il conto andrà in rosso! Non puoi procedere con questa operazione");
+    }
+
+    public String saldoFormattato(){
+        DecimalFormat saldoFormattato = new DecimalFormat("###,###,###.##");
+        return saldoFormattato.format(this.saldo);
+    }
+}
